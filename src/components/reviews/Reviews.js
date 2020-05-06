f0078aaaf5ad15553f0984823afb1e34d4144028
allison_johnson <allison.cuttler@gmail.com>
2020-05-06 12:35:29 -0700

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+  constructor(props) {
+    super(props)
+    console.log("props in Reviews: ", props)
+  }
+
+  renderReviews() {
+    return(this.props.reviews && this.props.reviews.map(review => <Review review={review} />))
+  }
+
   render() {
     return (
       <ul>
-        Reviews
+        {this.renderReviews()}
       </ul>
     );
   }
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -11,8 +11,8 @@ class ReviewsContainer extends Component {
   render() {
     return (
       <div>
-        <ReviewInput addReview={this.props.addReview} restaurant={this.props.restaurant} />
-        <Reviews />
+        <ReviewInput addReview={this.props.addReview} restaurantId={this.props.restaurant.id} />
+        <Reviews reviews={this.props.reviews} />
       </div>
     )
   }
